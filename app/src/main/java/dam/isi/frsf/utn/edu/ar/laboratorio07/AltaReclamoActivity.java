e3821f78cae1c218c371f2afe3d67c25047630fa
Daniel <d.campodonico@hotmail.com>
2016-11-15 02:10:11 -0300

    feat: Take picture

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio07/AltaReclamoActivity.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio07/AltaReclamoActivity.java
@@ -1,26 +1,42 @@
 package dam.isi.frsf.utn.edu.ar.laboratorio07;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AltaReclamoActivity extends AppCompatActivity {
 
     private Button btnCancelar;
     private Button btnAgregar;
+    private Button btnFoto;
     private EditText txtDescripcion;
     private EditText txtMail;
     private EditText txtTelefono;
     private LatLng ubicacion;
 	public static Integer CODIGO_RESULTADO_ALTA_RECLAMO = 1;
 	public static Integer CODIGO_RESULTADO_ALTA_RECLAMO_OK = 0;
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	static final int REQUEST_TAKE_PHOTO = 2;
+	private ImageView mImageView;
+	private String mCurrentPhotoPath;
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle extras = getIntent().getExtras();
@@ -31,12 +47,21 @@ public class AltaReclamoActivity extends AppCompatActivity {
         txtDescripcion = (EditText) findViewById(R.id.reclamoTexto);
         txtTelefono= (EditText) findViewById(R.id.reclamoTelefono);
         txtMail= (EditText) findViewById(R.id.reclamoMail);
+		btnFoto = (Button) findViewById(R.id.btnFoto);
+
+	    btnFoto.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+			    dispatchTakePictureIntent();
+		    }
+	    });
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Reclamo reclamo = new Reclamo(ubicacion.latitude, ubicacion.longitude,
 		            txtDescripcion.getText().toString().trim(), txtTelefono.getText().toString().trim(),
 		            txtMail.getText().toString().trim());
+	            reclamo.setImagenPath(mCurrentPhotoPath);
 	            Intent intent = getIntent();
 	            intent.putExtra("reclamo", reclamo);
 	            setResult(CODIGO_RESULTADO_ALTA_RECLAMO_OK, intent);
@@ -52,5 +77,50 @@ public class AltaReclamoActivity extends AppCompatActivity {
         });
     }
 
+	private void dispatchTakePictureIntent() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// Ensure that there's a camera activity to handle the intent
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			// Create the File where the photo should go
+			File photoFile = null;
+			try {
+				photoFile = createImageFile();
+			} catch (IOException ex) {
+				// Error occurred while creating the File
+			}
+			// Continue only if the File was successfully created
+			if (photoFile != null) {
+				Uri photoURI = FileProvider.getUriForFile(this,
+						"dam.isi.frsf.utn.edu.ar.laboratorio07.fileprovider",
+						photoFile);
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+				startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+			}
+		}
+	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+			Bundle extras = data.getExtras();
+			Bitmap imageBitmap = (Bitmap) extras.get("data");
+			mImageView.setImageBitmap(imageBitmap);
+		}
+	}
+
+	private File createImageFile() throws IOException {
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		String imageFileName = "JPEG_" + timeStamp + "_";
+		File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		File image = File.createTempFile(
+				imageFileName,  /* prefix */
+				".jpg",         /* suffix */
+				storageDir      /* directory */
+		);
+
+		// Save a file: path for use with ACTION_VIEW intents
+		mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+		return image;
+	}
 }
